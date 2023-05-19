e99afe1cc56c390ae8e9f3b600e116adb3b46aa6
jbljw02 <jbljw02@naver.com>
2023-05-19 21:19:54 +0900

    20230519 21:19

--- src/GC_leak.py
+++ src/GC_leak.py
@@ -1,12 +1,17 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        # 현재 노드가 다음 노드를 참조하지 않게 하여 메모리 누수 방지를 의도
+        self.next = None
 
-# '순환 참조' 구조를 가지는 함수
+# 순환 참조 구조를 이루는 함수 생성
 def circular_reference():
-
-    # a 리스트와 b 리스트가 서로 참조
-    a = [1]
-    b = [a]
-    a.append(b)
-    return a, b
+    node1 = Node(1)
+    node2 = Node(2)
+    # node1과 node2가 서로 참조하고 있으므로 GC의 대상 X
+    node1.next = node2
+    node2.next = node1
+    return node1, node2
 
 def main():
     while True:
